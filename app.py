daa8b7d6294554fe832283a673cead44705e5909
Kangyeol Lee <81941276+Kane-Lee@users.noreply.github.com>
2023-10-13 16:23:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    
+    @app.route("/")
+    def home():
+        return render_template("home.html")
+    
+    @app.route('/result', methods=['POST'])
+    def result():
+        gender = request.form.get('gender')
+        birthday = request.form.get('birthday')
+        weight = request.form.get('weight')
+        return f'gender : {gender} / birthday : {birthday} / weight : {weight}kg'
 
-@app.route("/")
-def home():
-    return render_template("home.html")
-
-@app.route('/result', methods=['POST'])
-def result():
-    gender = request.form.get('gender')
-    birthday = request.form.get('birthday')
-    weight = request.form.get('weight')
-    return f'gender : {gender} / birthday : {birthday} / weight : {weight}kg'
+    return app
