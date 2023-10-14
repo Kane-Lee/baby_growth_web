905c4651554e90042decce36630fdbacda975a15
kangyeollee <kyedlee@gmail.com>
2023-10-14 23:27:51 +0900

    add age in month

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template, request
+from datetime import datetime
+
+def calculate_month_age(birthday):
+    birthdate = datetime.strptime(birthday,'%Y-%m-%d')
+    current_date = datetime.now()
+    age_in_month = (current_date.year - birthdate.year) * 12 + (current_date.month - birthdate.month)
+    return age_in_month
 
 def create_app():
     app = Flask(__name__)
@@ -10,8 +17,13 @@ def create_app():
     @app.route('/result', methods=['POST'])
     def result():
         gender = request.form.get('gender')
+        if gender == "boy":
+            baby_gender = "남자"
+        else :
+            baby_gender = "여자"
         birthday = request.form.get('birthday')
+        month_age = calculate_month_age(birthday)
         weight = request.form.get('weight')
-        return f'gender : {gender} / birthday : {birthday} / weight : {weight}kg'
+        return render_template("result.html", age=month_age, gender=baby_gender)
 
     return app
---
+++ feature/month-calculator.py
@@ -0,0 +1,9 @@
+import datetime
+
+def create_age(birthday):
+    age=0
+    
+    formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+
+    
+    return age
